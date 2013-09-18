652fb9a22423d5e670af40df5acb885f8e0ced49
Joe Smongeski <jsmongeski@g.harvard.edu>
2013-09-17 22:29:12 -0400

    fix indentation

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 5.01//EN">
 <html>
-<head>
-  <title>Joe Smongeski's P1 Page</title>
-  <link rel="stylesheet" href="style.css">
-</head>
+   <head>
+      <title>Joe Smongeski's P1 Page</title>
+      <link rel="stylesheet" href="style.css">
+   </head>
 
-<body>
+   <body>
 
-<!-- Main content -->
-<h1>Joe Smongeski's P1 Page</h1>
-<!-- Site navigation menu -->
-<ul class="navbar">
-  <li><a href="index.php">P1 Home page</a>
-</ul>
-<p>Welcome to my dwa15 Project 1 page!
+      <!-- Main content -->
+      <h1>Joe Smongeski's P1 Page</h1>
+      <!-- Site navigation menu -->
+      <ul class="navbar">
+        <li><a href="index.php">P1 Home page</a>
+      </ul>
+      <p>Welcome to my dwa15 Project 1 page!
 
-<p>My name is Joe Smongeski.  Currently I work at Oracle in Bedford, MA as an SQA tools engineer.  We develop software test tools to facilitate automated testing of Oracle/Acme Packet IP communication products. (i.e. Session Border Controllers, Session Routers etc.).  Our tools are mainly written in tcl/expect, pretty much the de facto languages for SQA automation in datacomm companies, yet they are old and increasingly <i>less</i> utilized languages.  We also write in C and occasionally Java.  I'm taking dwa15 to catch up on the world of web programming, since it is increasingly becoming part of every software engineer's world!
+      <p>My name is Joe Smongeski.  Currently I work at Oracle in Bedford, MA as an SQA tools engineer.  We develop software test tools to facilitate automated testing of Oracle/Acme Packet IP communication products. (i.e. Session Border Controllers, Session Routers etc.).  Our tools are mainly written in tcl/expect, pretty much the de facto languages for SQA automation in datacomm companies, yet they are old and increasingly <i>less</i> utilized languages.  We also write in C and occasionally Java.  I'm taking dwa15 to catch up on the world of web programming, since it is increasingly becoming part of every software engineer's world!
 
-<p>That said, in a previous life, I earned a degree in music from Boston Conservatory, with a major in Classical Guitar.  I'm pretty rusty these days as having a full time job doesn't give me much time to practice. Maybe I'll pick the right
-  Powerball numbers soon so I can get back to playing Bach!  (Actually my second choice for a career was any computer-related work, so things have worked out ok!)
+      <p>That said, in a previous life, I earned a degree in music from Boston Conservatory, with a major in Classical Guitar.  I'm pretty rusty these days as having a full time job doesn't give me much time to practice. Maybe I'll pick the right
+      Powerball numbers soon so I can get back to playing Bach!  (Actually my second choice for a career was any computer-related work, so things have worked out ok!)
 
 
-<address>September 17, 2013<br><br>
-Acknowledgements for CSS help and review:<br>
-http://www.w3.org/Style/Examples/011/firstcss.en.html and <br>
-CSS Fundamentals at lynda.com.
-<br><br>Code editor: Sublime Text 2
-</address>
+      <address>September 17, 2013<br><br>
+      Acknowledgements for CSS help and review:<br>
+      http://www.w3.org/Style/Examples/011/firstcss.en.html and <br>
+      CSS Fundamentals at lynda.com.<br><br>
+      Code editor: Sublime Text 2
+      </address>
 
-</body>
+  </body>
 </html>
